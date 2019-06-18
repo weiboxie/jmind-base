746496a9a96a58f2d159c62ff0d7d45347b809f5
weibo.xwb <weibo.xwb@alibaba-inc.com>
2019-06-18 11:00:08 +0800

    增加 英文判断

--- src/main/java/jmind/base/util/RegexUtil.java
+++ src/main/java/jmind/base/util/RegexUtil.java
@@ -8,7 +8,7 @@ public class RegexUtil {
     // /^[A-Z_a-z0-9-\.]+@([A-Z_a-z0-9-]+\.)+[a-z0-9A-Z]{2,4}$/
     private static final String regEmail = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w]{2,4}$";
     private static final String regMobile = "^1(3|4|5|7|8)\\d{9}$";
-
+    private static final String regEnglish="^[a-zA-Z]+" ;
     public static boolean isEmail(String email) {
         return matcher(regEmail, email);
     }
@@ -20,11 +20,26 @@ public class RegexUtil {
     public static final boolean matcher(String regex, String value) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(value);
-        if (matcher.matches()) {
-            return true;
-        }
+        return  matcher.matches();
+    }
 
-        return false;
+    /**
+     * 是否全部字母
+     * @param charaString
+     * @return
+     */
+    public static boolean isEnglish(String charaString){
+        return matcher(regEnglish,charaString);
+    }
+
+    /**
+     * 是否全部中文
+     * @param str
+     * @return
+     */
+    public static boolean isChinese(String str){
+        return matcher(GlobalConstants.CHINESE_REGEX,str);
+
     }
 
 }
