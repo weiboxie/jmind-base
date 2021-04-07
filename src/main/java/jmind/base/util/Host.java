b64376e9f3065d709303ca0faaaf7f7eb53b2dd7
weibo <weibo.xwb@alibaba-inc.com>
2021-04-07 20:28:08 +0800

    http

--- src/main/java/jmind/base/util/Host.java
+++ src/main/java/jmind/base/util/Host.java
@@ -5,57 +5,52 @@ import java.util.Enumeration;
 
 public abstract class Host {
 
-    // export _webMaster=1 (master)
-    // export _webMaster=0 (slave)
-    public static boolean isMaster = "1".equals(System.getenv("_webMaster"));
 
+    private static String ip;
+    private static String hostName;
     private static InetAddress _host;
 
     static {
         try {
-            _host = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
+            // Init the host information.
+            resolveHost();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static boolean isMaster(String masterIp) {
-        return masterIp.equalsIgnoreCase(getHostIp());
+    private static void resolveHost() throws Exception {
+        _host = InetAddress.getLocalHost();
+        hostName = _host.getHostName();
+        ip = _host.getHostAddress();
+        if (_host.isLoopbackAddress()) {
+            // find the first IPv4 Address that not loopback
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface in = interfaces.nextElement();
+                Enumeration<InetAddress> addrs = in.getInetAddresses();
+                while (addrs.hasMoreElements()) {
+                    InetAddress address = addrs.nextElement();
+                    if (!address.isLoopbackAddress() && address instanceof Inet4Address) {
+                        ip = address.getHostAddress();
+                    }
+                }
+            }
+        }
     }
+
+    public static String getIp() {
+        return ip;
+    }
+
+    public static String getHostName() {
+        return hostName;
+    }
+
 
     public static InetAddress getHost() {
         return _host;
     }
 
-    public static String getHostName() {
-        try {
-            return _host.getHostName();
-        } catch (Exception e) {
-        }
-        return null;
-    }
-
-    public static String getHostIp() {
-        return _host.getHostAddress();
-//        if (_host.isLoopbackAddress()) {
-//            // find the first IPv4 Address that not loopback
-//            Enumeration<NetworkInterface> interfaces = null;
-//            try {
-//                interfaces = NetworkInterface.getNetworkInterfaces();
-//            } catch (SocketException e) {
-//                e.printStackTrace();
-//            }
-//            while (interfaces.hasMoreElements()) {
-//                NetworkInterface in = interfaces.nextElement();
-//                Enumeration<InetAddress> addrs = in.getInetAddresses();
-//                while (addrs.hasMoreElements()) {
-//                    InetAddress address = addrs.nextElement();
-//                    if (!address.isLoopbackAddress() && address instanceof Inet4Address) {
-//                        ip = address.getHostAddress();
-//                    }
-//                }
-//            }
-//        }
-    }
 
 }
